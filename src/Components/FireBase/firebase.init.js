baebff3f55ee23f7161dfc28bf51d745a0766912
Jahidul Islam <jahidul.islam114593@gmail.com>
2024-09-09 16:16:28 +0600

    [firebase secure]

--- src/Components/FireBase/firebase.init.js
+++ src/Components/FireBase/firebase.init.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA9a9yaTJUhKXkN-T8yzQLGePkJlMJ9Wa0",
-  authDomain: "glasses-auth-39338.firebaseapp.com",
-  projectId: "glasses-auth-39338",
-  storageBucket: "glasses-auth-39338.appspot.com",
-  messagingSenderId: "38815037701",
-  appId: "1:38815037701:web:a9951c438d405e02e85f6c",
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
